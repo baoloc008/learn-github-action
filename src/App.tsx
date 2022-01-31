46a21bd67701335126491720f4098a5f8ca5a9a2
baoloc008 <baoloc008@gmail.com>
2022-01-31 13:47:47 +0700

    update message

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>actions/setup-node@v2</p>
+        <p>actions/setup-node@v2 updated</p>
         <a
           className="App-link"
           href="https://reactjs.org"
