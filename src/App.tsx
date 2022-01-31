bf811cd6b382db21174913051fcd7ec00c0a5d0f
baoloc008 <baoloc008@gmail.com>
2022-01-31 13:41:04 +0700

    update action

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Updated.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>actions/setup-node@v2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
